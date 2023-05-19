73b2a32caa847e482ca78ac9314cf458b502ced8
denniswiersma <dennis.wiersma@icloud.com>
2023-05-19 16:45:23 +0200

    Setup assignment 1 file

---
+++ Assignment1/assignment1.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3.9
+
+"""
+Module description
+"""
+
+
+# IMPORTS
+
+# CLASSES
+
+# FUNCTIONS
+
+# MAIN
+def main():
+    """
+    Main function
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
