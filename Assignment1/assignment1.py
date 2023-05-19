48113f5d2532e50e9b2f933a4aeaefabf643dc16
denniswiersma <dennis.wiersma@icloud.com>
2023-05-19 20:09:24 +0200

    Implemented batch iterator method

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -41,6 +41,30 @@ class MeanPhredCalculator:
 
         return arg_parser.parse_args()
 
+    @staticmethod
+    def batch_iterator(iterator, batch_size):
+        """Returns lists of length batch_size.
+
+        This can be used on any iterator, for example to batch up
+        SeqRecord objects from Bio.SeqIO.parse(...), or to batch
+        Alignment objects from Bio.Align.parse(...), or simply
+        lines from a file handle.
+
+        This is a generator function, and it returns lists of the
+        entries from the supplied iterator.  Each list will have
+        batch_size entries, although the final list may be shorter.
+
+        Found at https://biopython.org/wiki/Split_large_file
+        """
+        batch = []
+        for entry in iterator:
+            batch.append(entry)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        if batch:
+            yield batch
+
 
 # FUNCTIONS
 
