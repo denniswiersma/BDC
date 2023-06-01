e63697adecb6ed2d85ccef84e59d517788603953
denniswiersma <dennis.wiersma@icloud.com>
2023-06-01 11:08:34 +0200

    setup assignment2 file

---
+++ Assignment2/assignment2.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3.9
+
+"""
+Module Docstring
+"""
+
+# IMPORTS
+
+# CLASSES
+
+# FUNCTIONS
+
+# MAIN
+
+def main():
+    """
+    Main function
+    """
+    return
+
+if __name__ == "__main__":
+    main()
